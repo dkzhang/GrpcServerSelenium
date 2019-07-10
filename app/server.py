095cd4f46c1c7793fe9cd7fcc357c42bae94a542
dkzhang@Office.PC <527656264@qq.com>
2019-07-10 09:18:50 +0800

    add selenium query in the server.

--- app/server.py
+++ app/server.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from concurrent import futures
 import time
 import logging
@@ -7,13 +9,29 @@ import grpc
 import the_pb2
 import the_pb2_grpc
 
+from selenium import webdriver
+firefox_capabilities ={
+    "browserName": "firefox",
+    "version": "",
+    "platform": "ANY",
+    "javascriptEnabled": True,
+    # "marionette": True,
+}
+
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
 class QueryID(the_pb2_grpc.QueryIDServicer):
 
     def Query(self, request, context):
-        return the_pb2.IdUrlReply(url='Hello, %s!' % request.isbn)
+        # return the_pb2.IdUrlReply(url='Hello, %s!' % request.isbn)
+        browser = webdriver.Remote("http://selenium-hub:4444/wd/hub", desired_capabilities=firefox_capabilities)
+        browser.get('https://book.douban.com/subject_search?search_text= 9787115445353&cat=1001/')
+        elem = browser.find_element_by_class_name("title-text")
+        book_url = elem.get_attribute('href')
+        print(book_url)
+        browser.quit()
+        return the_pb2.IdUrlReply(url=book_url)
 
 
 def serve():
